2a65df882f363d49a080fbbe619f2d5e78578dc6
JoelBenton <joelbenton1903@gmail.com>
2022-12-06 15:45:51 +0000

    Added Sub, Div, Mod to Py file

--- example.py
+++ example.py
@@ -3,3 +3,15 @@ def add(a,b):
 
 def multiply(a,b):
   return a*b
+
+def subtract(a,b):
+    return a-b
+
+def divide(a,b):
+    if (a == 0 or b == 0):
+        return 0
+    else:
+        return a/b
+
+def mod(a,b):
+    return a%b
--- test_example.py
+++ test_example.py
@@ -8,3 +8,15 @@ def test_add():
 def test_multiply():
     assert multiply(2, 1) == 2
     assert multiply(10, 5) == 50
+
+def test_subtract():
+    assert subtract(10, 5) == 5
+    assert subtract(5, 10) == -5
+
+def test_divide():
+    assert divide(0, 142) == 0
+    assert divide( 10, 2) == 5
+
+def test_mod():
+    assert mod(5, 3) == 2
+    assert mod(4, 2) == 0
