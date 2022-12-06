dae5c9555a86e57fb75ea16eb92dfe950ee39e99
JoelBenton <joelbenton1903@gmail.com>
2022-12-06 15:58:15 +0000

    Formatted Code using Make shell and black .

--- example.py
+++ example.py
@@ -1,17 +1,21 @@
-def add(a,b):
-  return a+b
+def add(a, b):
+    return a + b
 
-def multiply(a,b):
-  return a*b
 
-def subtract(a,b):
-    return a-b
+def multiply(a, b):
+    return a * b
 
-def divide(a,b):
-    if (a == 0 or b == 0):
+
+def subtract(a, b):
+    return a - b
+
+
+def divide(a, b):
+    if a == 0 or b == 0:
         return 0
     else:
-        return a/b
+        return a / b
 
-def mod(a,b):
-    return a%b
+
+def mod(a, b):
+    return a % b
--- test_example.py
+++ test_example.py
@@ -1,21 +1,26 @@
 from example import *
 
+
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -2) == -1
     assert add(1000000000000000000, 2) == 1000000000000000002
 
+
 def test_multiply():
     assert multiply(2, 1) == 2
     assert multiply(10, 5) == 50
+
 
 def test_subtract():
     assert subtract(10, 5) == 5
     assert subtract(5, 10) == -5
 
+
 def test_divide():
     assert divide(0, 142) == 0
-    assert divide( 10, 2) == 5
+    assert divide(10, 2) == 5
+
 
 def test_mod():
     assert mod(5, 3) == 2
